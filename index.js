1d798ed93a907cb49761d2dfebb9b31abace15a4
AffraAB <127426125+AffraAB@users.noreply.github.com>
2023-05-03 13:49:26 +0200

    Final push

--- index.js
+++ index.js
@@ -27,6 +27,27 @@ app.get('/bands', (req, res) => {
 
 // Get one
 app.get('/bands/:id', (req, res) => {
+    // Get single band from url parameter
     const band = db.prepare('SELECT * FROM bands WHERE id = ?').get(req.params.id);
+    // Return json or error
     res.json(band || {error: 'No such band'});
-})
+});
+
+// Create new band - POST
+app.post('/bands', (req, res) => {
+    // Log out to the console
+    console.log(req.body);
+    // Get name and genre from request body
+    const name = req.body.name;
+    const genre = req.body.genre;
+
+    // Query the database
+    const statement = db.prepare('INSERT INTO bands (name, genre) VALUES (?, ?)');
+    const result = statement.run(name, genre);
+
+    // Return result
+    res.json(result);
+});
+
+/* Start the app */
+app.listen(3000, () => { console.log('Server started on port 3000') });
